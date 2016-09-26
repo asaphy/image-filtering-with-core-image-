8251fb8c6980fa1bec7529f3227bff48e2a9091b
Asaph <asaph@intrepid.io>
2016-09-26 12:34:48 -0400

    added filtering

--- Image Filters Practice/ViewController.swift
+++ Image Filters Practice/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addFilter()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addFilter() {
+        
+        // filter with GPU instead of CPU
+        let openGLContext = EAGLContext(api: .openGLES3)
+        if let openGLContext = openGLContext {
+            
+            let context = CIContext(eaglContext: openGLContext)
+            
+            // ensures that imageView image is not nil
+            guard let image = imageView?.image, let cgimg = image.cgImage else {
+                print("imageView doesn't have an image!")
+                return
+            }
+            
+            // converts UIImage to CIImage
+            let coreImage = CIImage(cgImage: cgimg)
+            
+            // create new "CISepiaTone" filter
+            let filter = CIFilter(name: "CIColorInvert")
+            
+            // sets input image
+            filter?.setValue(coreImage, forKey: kCIInputImageKey)
+            
+            
+            // using kCIInputIntensityKey or kCIInputEVKey for certain filters will cause the app to crash
+            // sets intensity
+            // filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+            // filter?.setValue(1, forKey: kCIInputEVKey)
+            
+            // makes call to filter image
+            if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+                let cgimgresult = context.createCGImage(output, from: output.extent)
+                if let result = cgimgresult {
+                    let finishedImage = UIImage(cgImage: result)
+                    imageView?.image = finishedImage
+                }
+            } else {
+                print("image filtering failed")
+            }
+        }
     }
-
-
 }
 
